8fed730d389a37e7500907ef27a411a893da0e35
Yvan Sraka <yvan.sraka@gmail.com>
2016-07-05 16:19:36 +0200

    Update exercise

---
+++ bob.php
@@ -0,0 +1,23 @@
+<?php
+
+//
+// This is only a SKELETON file for the "Hamming" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+class Bob
+{
+    /**
+     * Respond to an input string
+     *
+     * @param string $str
+     * @return string
+     */
+    public function respondTo($str)
+    {
+        //
+        // YOUR CODE GOES HERE
+        //
+    }
+
+}
